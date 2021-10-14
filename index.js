26ef8d1198d2c0d86c4c9a37b5e67ee5d4427b1c
Cristofer <cristoferlorenzana@gmail.com>
2021-10-14 16:06:33 -0500

    commit tree 0b4e5cb6d1fd457ad6bbf39a198c1a35243d3a56

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+// function superbowlWin(wins){
+//     for(const element of wins){
+//         if (element.result === "W"){
+//             return element.year
+//         }
+//     }
+// }
 
+// function superbowlWin(win){
+//     return win.find(findWins)
+// }  
+
+// function findWins(element){
+//     if (element.result === "W"){
+//         return element.year;
+//     }
+// }
+
+function superbowlWin(record) {
+    const winObject = record.find(x => x.result === "W");
+    if (winObject == "undefined"){
+        return winObject
+    }else{
+        return winObject.year
+    }
+}
